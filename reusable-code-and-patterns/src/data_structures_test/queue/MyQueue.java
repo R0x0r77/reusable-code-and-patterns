32d9c238dcd5daade68af8d2f28365dbeb58784b
Kamil <kosiorek.kamil90@gmail.com>
2019-06-23 21:44:15 +0200

    queue with nodes

--- reusable-code-and-patterns/src/data_structures_test/queue/MyQueue.java
+++ reusable-code-and-patterns/src/data_structures_test/queue/MyQueue.java
@@ -1,26 +1,70 @@
 package data_structures_test.queue;
 
-import data_structures_test.node.Node;
+import data_structures_test.exceptions.MyException;
 
-public class MyQueue <T> {
+public class MyQueue<T extends Double> {	// extending double (which is stupid, because no generics is needed at all) just to create method sum
 
 	private Node<T> front = null;
 	private Node<T> rear = null;
 	private int elements = 0;
 	
-	public MyQueue() {}
-	
-	public void enqueue(T data) {
-		Node<T> last = new Node<>(data, null);
-		if (isEmpty()) {
-			rear = last;
-			front = last;
-		} else {
-//			rear = 
+	private static class Node<T> {
+		T data;
+		Node<T> next;
+		
+		private Node(T data, Node<T> next) {
+			this.data = data;
+			this.next = next;
 		}
 	}
 	
+	public MyQueue() {}
+	
+	public void enqueue(T data) {
+		Node<T> oldLast = rear;
+		rear = new Node<>(data, null);
+		if (isEmpty())
+			front = oldLast;
+		else
+			oldLast = rear.next;
+		elements++;
+	}
+	
+	public T dequeue() throws MyException {
+		if (isEmpty()) throw new MyException("queue is empty");
+		T data = front.data;
+		front = front.next;
+		elements--;
+		if (isEmpty())
+			rear = null;
+		return data;
+	}
+	
+	public T peek() throws MyException {
+		if (isEmpty()) throw new MyException("queue is empty");
+		return front.data;
+	}
+	
+	public T sum(int begin, int end) throws MyException {
+		if (elements < end) throw new MyException("queue has only " + elements + " elements");
+		if (begin < 1 || end < 1 || begin > end) throw new IllegalArgumentException("bad arguments");
+		Node<T> next = front;
+		Double sum = (double) 0;
+		for (int i = 1; i < begin; i++)
+			next = next.next;
+		for (int i = begin; i < end; i++) {
+			sum += next.data;
+			next = next.next;
+		}
+		return (T) sum;
+			
+	}
+	
 	public boolean isEmpty() {
-		return elements == 0;
+		return front == null;
+	}
+	
+	public int size() {
+		return elements;
 	}
 }
